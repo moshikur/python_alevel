3f82024d6792fe1e025428cbb282953e64dac9c2
moshikur <moshikur.rahman@gmail.com>
2021-06-20 19:37:45 +0600

    Update dictionary.py

--- a2/dictionary.py
+++ a2/dictionary.py
@@ -1,1 +1,12 @@
-
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+print(thisdict["brand"])
+print(thisdict)
+x = thisdict.keys()
+print(x) #before the change
+car["brand"] = "Jaguar"
+print(x) #after the change
