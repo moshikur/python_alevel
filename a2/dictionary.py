4f57fa959e656525af0c261e242e9c9ac9bcd79d
moshikur <moshikur.rahman@gmail.com>
2021-06-20 19:41:06 +0600

    Update dictionary.py

--- a2/dictionary.py
+++ a2/dictionary.py
@@ -10,3 +10,17 @@ x = thisdict.keys()
 print(x) #before the change
 car["brand"] = "Jaguar"
 print(x) #after the change
+x = thisdict.values()
+
+thisdict2 = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+
+for k,v in thisdict2.items():
+  print(k)
+  for items in v:
+    print(items)
